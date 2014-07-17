53ca1e2c899e44c5995631c76fb96f92f6654a35
root <msniff16@gmail.com>
2014-07-17 04:44:36 -0400

    mobile api

---
+++ mobileapi.php
@@ -0,0 +1,14 @@
+<?php
+
+include('lib/forecast.io.php');
+include('twilio/sms-request.php');
+require('db_connection.php');
+
+/* TO DO */
+//user sign up function
+
+
+//user welcome text message
+
+
+//inital script run for user on sign up
